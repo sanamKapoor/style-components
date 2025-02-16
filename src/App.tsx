b0ab8ed632c2c38ac705427310dcc7c81dad8425
sanam-kapoor-md <sanam@alturalabz.com>
2025-02-16 16:52:48 +0530

    added features

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { theme } from './styles/theme';
 import { FormDemo } from './components/Form';
 import { ButtonDemo } from './components/ButtonDemo';
 import { BadgeDemo } from './components/BadgeDemo';
+import Link from './components/Link';
+import { StyledLink } from './styles/Link';
+import { Flex, Grid } from './styles/Layout';
 
 function App() {
     return (
@@ -14,12 +17,18 @@ function App() {
             <GlobalStyles />
             <Header />
             <Container>
-                <h1>Modern Styled Components</h1>
-                <p>A starter template for 2025 and beyond!</p>
+                <Flex direction="column" gap="1rem">
+                    <h1>Modern Styled Components</h1>
+                    <p>A starter template for 2025 and beyond!</p>
+                    <Grid columns={2} gap="1rem">
+                        <Link className="link">Hello</Link>
+                        <StyledLink className="link">Hello</StyledLink>
+                        <AnimatedLogo src="https://static-00.iconduck.com/assets.00/react-icon-2048x2048-o8k3ymqa.png" />
+                    </Grid>
+                </Flex>
                 <ButtonDemo />
                 <FormDemo />
                 <BadgeDemo />
-                <AnimatedLogo src="https://static-00.iconduck.com/assets.00/react-icon-2048x2048-o8k3ymqa.png" />
             </Container>
         </ThemeProvider>
     );
